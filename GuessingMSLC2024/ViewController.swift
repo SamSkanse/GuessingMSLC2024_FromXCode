98768d19df528282915bb88fd76dfd7228c04704
SamSkanse <sskanse@smu.edu>
2024-10-07 22:29:41 -0500

    Initial Commit

---
+++ GuessingMSLC2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessingMSLC2024
+//
+//  Created by Sam Skanse on 10/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GuessingMSLC2024Tests/GuessingMSLC2024Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingMSLC2024Tests.swift
+//  GuessingMSLC2024Tests
+//
+//  Created by Sam Skanse on 10/7/24.
+//
+
+import Testing
+@testable import GuessingMSLC2024
+
+struct GuessingMSLC2024Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
