4fb60055f85f39f162ac5c344e159772bf543a85
SamSkanse <sskanse@smu.edu>
2024-10-07 23:23:18 -0500

    shjsd

--- GuessingMSLC2024/ViewController.swift
+++ GuessingMSLC2024/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var HelloWorldLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        HelloWorldLabel.text = "Idiot"
     }
 
-
+    @IBAction func Button(_ sender: Any) {
+    }
+    
 }
 
